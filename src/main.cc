e47072b6739be0d2924851976db97714d20edfca
Michael Yang <ohmyarchlinux@gmail.com>
2016-05-03 16:19:36 +0800

    Update

--- src/main.cc
+++ src/main.cc
@@ -2,7 +2,7 @@
 
 int main(int argc, char *argv[]) {
     ucout << web::json::value::parse(
-                 U("{\"test\":\"\\ud83c\\uddee\\ud83c\\udde9\"}"))
+                 U("{\"test\":\"\\uff1a\"}"))
                  .at(U("test"))
                  .as_string()
           << std::endl;
